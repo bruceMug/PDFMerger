1ff431cc63a20ae427ca316068e685a906ac0f8c
bruceMug <mugzbru199@gmail.com>
2023-05-15 18:58:52 +0300

    created file reader objects

--- merger.py
+++ merger.py
@@ -5,3 +5,7 @@ pdf1 = open('file1.pdf', 'rb')
 
 #open other pdf
 pdf2 = open('file2.pdf', 'rb')
+
+#create a reader object
+reader1 = PyPDF2.PdfFileReader(pdf1)
+reader2 = PyPDF2.PdfFileReader(pdf2)
